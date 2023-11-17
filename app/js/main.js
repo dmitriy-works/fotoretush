516cdfa1e0cdb68172bf209352518af7006207a9
dkhimich <dmitriy-works@ukr.net>
2023-11-17 19:23:35 +0300

    change logo and start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -32,12 +32,26 @@ const socialnetwork__swiper = new Swiper('.socialnetwork__swiper', {
     el: '.socialnetwork__pagination',
     clickable: true,
   },
-
-  // Navigation arrows
+  
   navigation: {
     nextEl: '.socialnetwork-button-right',
     prevEl: '.socialnetwork-button-left',
   },
-
   
 });
+
+const menuBtn = document.querySelector('.burger__menu');
+const menu = document.querySelector('.menu__list');
+const logoWhite = document.querySelector('.header__logo-white');
+const logoBlack = document.querySelector('.header__logo');
+const burgerMenu = document.querySelector('.burger__span');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+  logoWhite.classList.toggle('logo__active');
+  logoBlack.classList.toggle('logo__black--active');
+  burgerMenu.classList.toggle('burger__span--active');
+});
+
+
+
